ea0126bf2a4052f1c8947a25bd7381abc50fb6f1
Band-hi <bandisalazar@outlook.com>
2021-07-06 15:59:44 -0500

    Form Login

---
+++ REMI V2.0/frmLogin.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace REMI_V2._0 {
+    partial class frmLogin {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtUsuarioEmail = new TextoBoxAutoExplicativo.txtAutoExplicativo();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtPassword = new TextoBoxAutoExplicativo.txtAutoExplicativo();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelRegistro = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(115, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SF Pro Display", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "REMI";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtUsuarioEmail);
+            this.groupBox1.Font = new System.Drawing.Font("SF Pro Display", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 230);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 55);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Usuario o Email";
+            // 
+            // txtUsuarioEmail
+            // 
+            this.txtUsuarioEmail.BackColor = System.Drawing.Color.White;
+            this.txtUsuarioEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsuarioEmail.ColorTextoVacio = System.Drawing.Color.Gray;
+            this.txtUsuarioEmail.Font = new System.Drawing.Font("SF Pro Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuarioEmail.Location = new System.Drawing.Point(6, 21);
+            this.txtUsuarioEmail.Multiline = true;
+            this.txtUsuarioEmail.Name = "txtUsuarioEmail";
+            this.txtUsuarioEmail.Size = new System.Drawing.Size(338, 27);
+            this.txtUsuarioEmail.TabIndex = 0;
+            this.txtUsuarioEmail.TextoVacio = "Ingrese su usuario o email";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtPassword);
+            this.groupBox2.Font = new System.Drawing.Font("SF Pro Display", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 300);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(350, 55);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Contraseña";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.White;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.ColorTextoVacio = System.Drawing.Color.Gray;
+            this.txtPassword.Font = new System.Drawing.Font("SF Pro Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(6, 21);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(338, 27);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.TextoVacio = "Ingrese su contraseña";
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("SF Pro Display", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(162, 386);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "¿Olvidaste tu contraseña?";
+            // 
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(161)))), ((int)(((byte)(203)))));
+            this.btnIniciarSesion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciarSesion.Font = new System.Drawing.Font("SF Pro Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarSesion.ForeColor = System.Drawing.Color.White;
+            this.btnIniciarSesion.Location = new System.Drawing.Point(13, 497);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(350, 50);
+            this.btnIniciarSesion.TabIndex = 5;
+            this.btnIniciarSesion.Text = "Iniciar Sesion";
+            this.btnIniciarSesion.UseVisualStyleBackColor = false;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("SF Pro Display", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(9, 615);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "¿No tienes una cuenta?";
+            // 
+            // labelRegistro
+            // 
+            this.labelRegistro.AutoSize = true;
+            this.labelRegistro.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelRegistro.Font = new System.Drawing.Font("SF Pro Display", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRegistro.Location = new System.Drawing.Point(277, 615);
+            this.labelRegistro.Name = "labelRegistro";
+            this.labelRegistro.Size = new System.Drawing.Size(85, 19);
+            this.labelRegistro.TabIndex = 7;
+            this.labelRegistro.Text = "Registrate";
+            this.labelRegistro.Click += new System.EventHandler(this.labelRegistro_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(374, 661);
+            this.Controls.Add(this.labelRegistro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnIniciarSesion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "REMI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private TextoBoxAutoExplicativo.txtAutoExplicativo txtUsuarioEmail;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private TextoBoxAutoExplicativo.txtAutoExplicativo txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelRegistro;
+    }
+}
+
