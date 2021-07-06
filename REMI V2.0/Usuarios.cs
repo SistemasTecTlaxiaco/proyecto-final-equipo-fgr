94b916c2fe94464867a80d0c529c75a8647273a9
Band-hi <bandisalazar@outlook.com>
2021-07-06 15:54:49 -0500

    Clase Usuarios

---
+++ REMI V2.0/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REMI_V2._0 {
+    class Usuarios {
+        int idUsuario;
+        string nombre, usuario, email, palabraSeguridad, password, conPassword;
+
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string Email { get => email; set => email = value; }
+        public string PalabraSeguridad { get => palabraSeguridad; set => palabraSeguridad = value; }
+        public string Password { get => password; set => password = value; }
+        public string ConPassword { get => conPassword; set => conPassword = value; }
+    }
+}
